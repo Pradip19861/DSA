079f26eb9fa9a9dd1a3844b4c0d93d2d5ccf6e99
PRADIP <pradipdolai03@gmail.com>
2024-10-03 23:10:54 +0530

    nn

---
+++ Accenture/TCS/armstrongnum.py
@@ -0,0 +1,15 @@
+def armstrong(n):
+    sum = 0
+    temp = n
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** 3
+        temp //= 10
+    return sum == n
+
+n = int(input("Enter a number: "))
+
+if armstrong(n):
+    print(n, "is an Armstrong number.")
+else:
+    print(n, "is not an Armstrong number.")
